6fac5db3921d0fac51b6100f5da93bd781f00948
Teotonio José de Souza Junior <102707827+teotoniojr@users.noreply.github.com>
2024-04-02 08:57:18 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 const botoes = document.querySelectorAll('.botao');
 const textos = document.querySelectorAll('.aba-conteudo')
 
+const contadores = document.querySelectorAll('.contador')
+
+const tempoObjetivo01 = new Date("2024-04-03T00:00:00")
+const tempoObjetivo02 = new Date("2024-06-25T00:00:00")
+const tempoObjetivo03 = new Date("2024-07-15T00:00:00")
+const tempoObjetivo04 = new Date("2024-07-06T00:00:00")
+
+const tempos = [tempoObjetivo01, tempoObjetivo02, tempoObjetivo03, tempoObjetivo04];
+
+for (let i= 0; i<tempos.length; i++){
+    contadores[i].textContent = calculaTempo(tempos[i])
+}
+
+
 for(let i= 0; i<botoes.length; i++){
     botoes[i].onclick = function (){
 
@@ -12,4 +26,22 @@ for(let i= 0; i<botoes.length; i++){
         botoes[i].classList.add('ativo')
         textos[i].classList.add('ativo')
     } 
+}
+
+function calculaTempo (tempoObjetivo) {
+
+    let tempoAtual = new Date()
+    let tempoFinal = tempoObjetivo - tempoAtual
+
+    let segundos = Math.floor(tempoFinal/1000);
+    let minutos = Math.floor(segundos/60);
+    let horas = Math.floor(minutos/60);
+    let dias = Math.floor(horas/24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+
+    return dias + " Dias " + horas + " Horas " + minutos + " Minutos " + segundos + " Segundos "
+
 }
