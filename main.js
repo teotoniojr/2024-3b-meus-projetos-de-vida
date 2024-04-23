0062f8efb5d953035cf8e71cbe0c7c148b32d8ed
Teotonio José de Souza Junior <102707827+teotoniojr@users.noreply.github.com>
2024-04-23 08:04:16 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -10,38 +10,49 @@ const tempoObjetivo04 = new Date("2024-07-06T00:00:00")
 
 const tempos = [tempoObjetivo01, tempoObjetivo02, tempoObjetivo03, tempoObjetivo04];
 
-for (let i= 0; i<tempos.length; i++){
-    contadores[i].textContent = calculaTempo(tempos[i])
+function atualizaCronometro() {
+    for (let i = 0; i < tempos.length; i++) {
+        contadores[i].textContent = calculaTempo(tempos[i])
+    }
 }
 
+function comecaCronometro() {
+    atualizaCronometro();
+    setInterval(atualizaCronometro, 1000);
+}
 
-for(let i= 0; i<botoes.length; i++){
-    botoes[i].onclick = function (){
+//comecaCronometro();
 
-        for(j=0; j<botoes.length; j++){
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+
+        for (j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove('ativo')
             textos[j].classList.remove('ativo')
         }
 
         botoes[i].classList.add('ativo')
         textos[i].classList.add('ativo')
-    } 
+    }
 }
 
-function calculaTempo (tempoObjetivo) {
+function calculaTempo(tempoObjetivo) {
 
     let tempoAtual = new Date()
     let tempoFinal = tempoObjetivo - tempoAtual
 
-    let segundos = Math.floor(tempoFinal/1000);
-    let minutos = Math.floor(segundos/60);
-    let horas = Math.floor(minutos/60);
-    let dias = Math.floor(horas/24);
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
 
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
 
-    return dias + " Dias " + horas + " Horas " + minutos + " Minutos " + segundos + " Segundos "
-
+    if (tempoFinal > 0) {
+        return dias + " Dias " + horas + " Horas " + minutos + " Minutos " + segundos + " Segundos "
+    } else {
+        return "Prazo finalizado"
+    }
 }
